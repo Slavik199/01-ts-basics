22477a9ca44effac423c7b072f70b232dc2f3b74
Slavik199 <rizikovslavik@gmail.com>
2025-10-26 20:06:33 +0200

    hw-2-ts

--- src/task-6.ts
+++ src/task-6.ts
@@ -2,13 +2,12 @@ function getFirstElement<T>(arr: T[]): T {
   return arr[0];
 }
 
+
 const num = getFirstElement<number>([1, 2, 3]);
 const str = getFirstElement<string>(["a", "b", "c"]);
 const bool = getFirstElement<boolean>([true, false, true]);
 
 
-
-
-getFirstElement([1, 2, 3]);           // 1
-getFirstElement(["a", "b", "c"]);     // "a"
-getFirstElement([true, false, true]); // true
+getFirstElement<number>([1, 2, 3]);           // 1
+getFirstElement<string>(["a", "b", "c"]);     // "a"
+getFirstElement<boolean>([true, false, true]); // true
